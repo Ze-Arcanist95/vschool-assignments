f773d3292224f3041112f53b875c3c3908ffd8da
Zachary Arcane <zarcanist95@gmail.com>
2021-01-14 09:29:47 -0500

    Clear & Fun Button - Working

--- dom-olympics-start/index.js
+++ dom-olympics-start/index.js
@@ -11,7 +11,12 @@ const creditSpan = document.createElement("span");
 const h3 = document.createElement("h3");
 const clrBtn = document.getElementById("clear-button");
 const sndBtn = document.getElementById("send-button");
-const userInput = document.getElementById("input");
+const funBtn = document.getElementById("fun-button")
+const msgForm = document.getElementById("message-line");
+const msg = document.getElementsByClassName("message");
+const msgs = document.querySelector(".messages");
+    
+
 
 h1.classList.add("header");
 h1.textContent = "JavaScript Made This!!";
@@ -23,9 +28,21 @@ creditSpan.textContent = " wrote the JavaScript";
 creditSpan.classList.add("span");
 
 h3.classList.add("header");
+h3.style.flexDirection = "row";
 h3.append(nameSpan);
 h3.append(creditSpan);
-h3.style.flexDirection = "row";
 
 header.append(h1);
 header.append(h3);
+
+funBtn.addEventListener('click', () => {
+    msg[3].textContent = "Good";
+    msg[2].textContent = "and";
+    msg[1].textContent = "Fun";
+    msg[0].textContent = "Something";
+});
+
+clrBtn.addEventListener('click', () => {
+    msgs.innerHTML = "";
+});
+
