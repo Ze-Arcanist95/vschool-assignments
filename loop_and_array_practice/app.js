8a42c93f3a1e33230927da98bc343867c6a8f425
Zachary Arcane <zarcanist95@gmail.com>
2021-01-19 17:18:58 -0500

    tweak

--- loop_and_array_practice/app.js
+++ loop_and_array_practice/app.js
@@ -14,63 +14,76 @@
     */
 
 //Loop through the following array and log to the console "old enough" if they are 18 or older, and "not old enough" if thy aren't 18.
-
-    var peopleWhoWantToSeeMadMaxFuryRoad = [
-        {
-            name: "Mike",
-            age: 12,
-            gender: "male"
-        },{
-            name: "Madeline",
-            age: 80,
-            gender: "female"
-        },{
-            name: "Cheryl",
-            age: 22,
-            gender: "female"
-        },{
-            name: "Sam",
-            age: 30,
-            gender: "male"
-        },{
-            name: "Suzy",
-            age: 4,
-            gender: "female"
-        }
-    ];
-
+    
     /*
-        for(i = 0; i < peopleWhoWantToSeeMadMaxFuryRoad.length; i++){
-            if(peopleWhoWantToSeeMadMaxFuryRoad[i].age >= 18) {
-                console.log("Old Enough");
-            } else {
-                console.log("Not Old Enough");
+        var peopleWhoWantToSeeMadMaxFuryRoad = [
+            {
+                name: "Mike",
+                age: 12,
+                gender: "male"
+            },{
+                name: "Madeline",
+                age: 80,
+                gender: "female"
+            },{
+                name: "Cheryl",
+                age: 22,
+                gender: "female"
+            },{
+                name: "Sam",
+                age: 30,
+                gender: "male"
+            },{
+                name: "Suzy",
+                age: 4,
+                gender: "female"
             }
-        }
+        ];
     */
-    /*
-        for(i = 0; i < peopleWhoWantToSeeMadMaxFuryRoad.length; i++){
-            if(peopleWhoWantToSeeMadMaxFuryRoad[i].age >= 18) {
-                console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is old enough to see Mad Max");
-            } else {
-                console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is not old enough to see Mad Max");
-            }
-        }
-    */
-    /*
-        for(i = 0; i < peopleWhoWantToSeeMadMaxFuryRoad.length; i++){
-            if(peopleWhoWantToSeeMadMaxFuryRoad[i].age >= 18) {
-                if(peopleWhoWantToSeeMadMaxFuryRoad[i].gender === "female"){
-                    console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is old enough. She's good to see Mad Max.");
+        /*
+            for(i = 0; i < peopleWhoWantToSeeMadMaxFuryRoad.length; i++){
+                if(peopleWhoWantToSeeMadMaxFuryRoad[i].age >= 18) {
+                    console.log("Old Enough");
                 } else {
-                    console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is old enough. He's good to see Mad Max.");
-                }
-            } else {
-                if(peopleWhoWantToSeeMadMaxFuryRoad[i].gender === "female"){
-                    console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is not old enough to see Mad Max, don't let her in.");
-                } else {
-                    console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is not old enough to see Mad Max, don't let him in.");
+                    console.log("Not Old Enough");
                 }
             }
-        }
-    */
+        */
+        /*
+            for(i = 0; i < peopleWhoWantToSeeMadMaxFuryRoad.length; i++){
+                if(peopleWhoWantToSeeMadMaxFuryRoad[i].age >= 18) {
+                    console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is old enough to see Mad Max");
+                } else {
+                    console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is not old enough to see Mad Max");
+                }
+            }
+        */
+        /*
+            for(i = 0; i < peopleWhoWantToSeeMadMaxFuryRoad.length; i++){
+                if(peopleWhoWantToSeeMadMaxFuryRoad[i].age >= 18) {
+                    if(peopleWhoWantToSeeMadMaxFuryRoad[i].gender === "female"){
+                        console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is old enough. She's good to see Mad Max.");
+                    } else {
+                        console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is old enough. He's good to see Mad Max.");
+                    }
+                } else {
+                    if(peopleWhoWantToSeeMadMaxFuryRoad[i].gender === "female"){
+                        console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is not old enough to see Mad Max, don't let her in.");
+                    } else {
+                        console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is not old enough to see Mad Max, don't let him in.");
+                    }
+                }
+            }
+        */
+/*
+Imagine you have a button that toggles a light on and off. Loop through the following array of numbers and toggle the button the numbers of times for each number. The array [2, 3, 2] would toggle the button 7 times.
+
+The light is off to start with. Log to the console whether or not the light is on at the end.
+       
+Sample Arrays:
+
+[2, 5, 435, 4, 3] // "The light is on"
+[1, 1, 1, 1, 3]   // "The light is on"
+[9, 3, 4, 2]      // "The light is off"
+*/
+    
