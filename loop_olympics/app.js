9feba042cbf638f6dca97e166ccd32a0c7628227
Zachary Arcane <zarcanist95@gmail.com>
2021-01-20 08:54:28 -0500

    Prelims and Bronze Medal - Comple Medal Complete

---
+++ loop_olympics/app.js
@@ -0,0 +1,45 @@
+/*
+// Preliminaries
+// Write a for loop that prints to the console the numbers 0 through 9.
+    for(i = 0; i < 10; i++){
+        console.log(i);
+    };
+
+// Write a for loop that prints to the console 9 through 0.
+    for(i = 10; i > 0; i--){
+        console.log(i);
+    };
+// Write a for loop that prints these fruits to the console.
+    var fruit = ["banana", "orange", "apple", "kiwi"]
+    for(i = 0; i < fruit.length; i++){
+        console.log(fruit[i]);
+    };
+
+*/
+/*
+// Bronze Medal
+// Write a for loop that will push the numbers 0 through 9 to an array.
+    let numArr = [];
+    for(i = 0; i < 10; i++){
+        numArr.push(i);
+        console.log(numArr);
+    };
+
+// Write a for loop that prints to the console only even numbers 0 through 100.
+    for(i = 0; i <= 100; i++){
+        if(i % 2 === 0){
+            console.log(i);
+        };
+    };
+
+// Write a for loop that will push every other fruit to an array.
+    var fruit = ["banana", "orange", "apple", "kiwi", "pear", "peach"];
+    var fruit2 = [];
+    for(i = 0; i < fruit.length; i++){
+        if(i % 2 !== 0){
+            fruit2.push(fruit[i])
+            console.log(fruit2)
+        }
+    }
+*/
+
