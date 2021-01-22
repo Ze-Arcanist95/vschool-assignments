d2b745c9a33f9f90e6731fc02c1d34fb80fd4e84
Zachary Arcane <zarcanist95@gmail.com>
2021-01-22 12:12:46 -0500

    Forception - Complete

---
+++ forception/app.js
@@ -0,0 +1,16 @@
+let people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"];
+let alphabet = ["abcdefghijklmnopqrstuvwxyz"];
+
+function forception(people,alphabet){
+    let funcOutput = []
+    for(i = 0; i < arguments[0].length; i++){
+        people.shift(funcOutput);
+        for(i2 = 0; i2 < arguments[1].length; i++){
+            alphabet.toUpperCase();
+            let newAlpha = alphabet.split("");
+            funcOutput.concat(newAlpha);
+        };
+    };
+    console.log(funcOutput);
+};
+forception(people, alphabet);
