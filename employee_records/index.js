ec45760f36358d4a9438fcba0c2bde1373a2af0c
Zachary Arcane <zarcanist95@gmail.com>
2021-01-22 22:52:57 -0500

    crash - security commit

--- employee_records/index.js
+++ employee_records/index.js
@@ -11,9 +11,9 @@ Employee.prototype.printEmployeeForm = () => {
     console.log(this.name + this.title + this.salary + this.status);
 };
 
-let amanda = employees.push( new Employee("Amanda", "Bartender", "$36,000", "Part-Time"));
-let phillip = employees.push( new Employee("Phillip", "Sue Chef", "$42,000", "Contract"));
-let valerie = employees.push( new Employee("Valerie", "General Manager", "$60,000"));
+let person1 = employees.push( new Employee("Amanda", "Bartender", "$36,000", "Part-Time"));
+let person2 = employees.push( new Employee("Phillip", "Sue Chef", "$42,000", "Contract"));
+let person3 = employees.push( new Employee("Valerie", "General Manager", "$60,000"));
 
 console.log(employees);
 
