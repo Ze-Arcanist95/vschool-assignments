b47de8900daf4231e800a0f64936b03d1630f971
Zachary Arcane <zarcanist95@gmail.com>
2021-01-23 00:49:47 -0500

    update again. no major changes from this or prior commit.

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -1,9 +1,4 @@
 for(let i = 0; i <= 100; i++) {
-    let count = {
-        FizzBuzz = console.count(),
-        Fizz = console.count(),
-        Buzz = console.count()
-    }
     if(i % 3 === 0){
         console.log("Fizz");
     } else if (i % 5 === 0) {
