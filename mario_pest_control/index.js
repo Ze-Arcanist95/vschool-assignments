56010eba8d4d7374699f0e6c3f739f77c9401b60
Zachary Arcane <zarcanist95@gmail.com>
2021-01-28 18:29:53 -0500

    Updating Tracker Form - Working / Project Complete

--- mario_pest_control/index.js
+++ mario_pest_control/index.js
@@ -9,7 +9,18 @@ submitBtn.addEventListener("click", (e) => {
 
     let goombas = document.pestTracker.goombaInput.value;
     let bombombs = document.pestTracker.bombInput.value;
-    let cheeps = document.pestTracker.bombInput.value;
+    let cheeps = document.pestTracker.cheepInput.value;
+    let newGoomba = parseInt(goombaCount.textContent) + parseInt(goombas);
+    let goombaPrice = newGoomba * 5;
+    let newBombomb = parseInt(bombCount.textContent) + parseInt(bombombs);
+    let bombPrice = newBombomb * 7;
+    let newCheep = parseInt(cheepCount.textContent) + parseInt(cheeps);
+    let cheepPrice = newCheep * 11;
 
-    goombaCount.textContent = (parseInt(goombaCount.textContent) + goombas);
+    goombaCount.textContent = newGoomba + " / " + goombaPrice + " Coins";
+    bombCount.textContent = newBombomb + " / " + bombPrice + " Coins";
+    cheepCount.textContent = newCheep + " / " + cheepPrice + " Coins";
+
+    bill.textContent = (goombaPrice + bombPrice + cheepPrice) + " Coins";
+
 })
