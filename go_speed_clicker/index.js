795e00ce9584dedfd69121f84aac3bdb42edf3a8
Zachary Arcane <zarcanist95@gmail.com>
2021-02-01 14:37:13 -0500

    Daily Commit / Changed around variables, values, and application of click event

--- go_speed_clicker/index.js
+++ go_speed_clicker/index.js
@@ -1,17 +1,20 @@
-localStorage.setItem("count", 0);
-
 const timerBtn = document.getElementById('timer-button');
 const resetBtn = document.getElementById('reset-button')
 const clickCount = document.getElementById('click-count');
 const clickMe = document.getElementById("click-me");
 let isTimed = false;
 let timeLeft = 15;
-let countVar = localStorage.getItem("count")
-
-clickCount.innerHTML = countVar;
-
-// while(isTimed === true) {}
-
+localStorage.setItem("clicks", "0");
+  let count = localStorage.getItem("clicks");
+clickCount.textContent = count;
+while(isTimed === true) {
+  clickMe.addEventListener("click", () => {
+    let clicks = parseInt(count);
+    localStorage.setItem("clicks", ++clicks)
+    clickCount.textContent = count
+    console.log("clicked")
+  });
+}
 function startTimer() {
     if(timeLeft === 0){
       timerBtn.textContent = "Start";
@@ -22,9 +25,6 @@ function startTimer() {
       isTimed = true;
       setTimeout(startTimer, 1000);
     }
-}
-function clicker() {
-  countVar++;
 }
 function reset() {
     timeLeft = 15;
