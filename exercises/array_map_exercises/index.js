9916731bbe71b404e73fdd05ff3dde82ec9e2285
Zachary Arcane <zarcanist95@gmail.com>
2021-02-02 13:06:07 -0500

    Second exercise complete

--- exercises/array_map_exercises/index.js
+++ exercises/array_map_exercises/index.js
@@ -1,5 +1,13 @@
+/*
 function doubleNumbers(arr) {
     return arr.map(num => num * 2);
 }
 
 console.log(doubleNumbers([1, 2, 3, 4, 5]));
+*/
+
+function stringItUp(arr) {
+    return arr.map(String);
+}
+  
+console.log(stringItUp([2, 5, 100]));
