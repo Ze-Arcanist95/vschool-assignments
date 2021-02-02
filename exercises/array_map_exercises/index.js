e4ef8115132a104cde7db7802a9f08caec02a64f
Zachary Arcane <zarcanist95@gmail.com>
2021-02-02 13:45:23 -0500

    Third exercise complete

--- exercises/array_map_exercises/index.js
+++ exercises/array_map_exercises/index.js
@@ -5,9 +5,20 @@ function doubleNumbers(arr) {
 
 console.log(doubleNumbers([1, 2, 3, 4, 5]));
 */
-
+/*
 function stringItUp(arr) {
     return arr.map(String);
 }
   
 console.log(stringItUp([2, 5, 100]));
+*/
+/*
+function capitalizeNames(arr){
+    return arr.map(index => {
+        return index.charAt(0).toUpperCase() + index.slice(1).toLowerCase();
+    });
+}
+  
+console.log(capitalizeNames(["john", "JACOB", "jinGleHeimer", "schmidt"]));
+*/
+
