bf4eb0abbd889cdf97b888fa080b36f30aeca6ec
Zachary Arcane <zarcanist95@gmail.com>
2021-02-04 14:25:56 -0500

    Catch Me (Error Handling) - Complete

---
+++ catch_me_if_you_can/index.js
@@ -0,0 +1,34 @@
+// 1a) Write a function that returns the sum of two numbers. Throw an error if either argument is not of the data type number:
+// 1b) Call the sum function inside a try block using "1" and "2" as arguments. Use console.log within a catch block to inform the user of the error.
+
+function sum(x, y){
+    if (typeof x !== "number" || typeof y !== "number"){
+        throw "NaN"
+    }
+    return x + y;
+}
+try{
+    sum("1", "2")
+}
+catch(err){
+    console.log(err)
+}
+
+// 2a) Given a user object, write a function called login that takes a username and password as parameters. Throw an error if either of them don't match. Otherwise, log to the console a message saying "login successful!"
+// 2b) Call the login function within a try block. In one instance use the correct credentials, and in another use incorrect ones. Make sure you see the appropriate message!
+
+var user = {username: "sam", password: "123abc"};
+function login(username, password){
+    if (username !== user.username || password !== user.password){
+        throw "username/password combination is incorrect.";
+    } else {
+        console.log("login successful!");
+    }
+}
+try {
+    login("sam", "123abc");
+    login("mas", "cba321");
+}
+catch(err){
+    console.log(err);
+}
